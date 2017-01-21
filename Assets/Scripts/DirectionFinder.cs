228cb0ed332238532c9fc45afb25658dcaf7dc06
Ramil Lim <ramil.lim@gmail.com>
2017-01-21 09:59:58 -0500

    Check in test scene.

---
+++ Assets/Scripts/DirectionFinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DirectionFinder : MonoBehaviour
+{
+	private GameObject target;
+	public Text distanceText;
+	public Text directionText;
+
+	// Use this for initialization
+	void Start()
+	{
+		target = GameObject.FindWithTag("Target");
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var targetPos = target.transform.position;
+
+		var heading = targetPos - transform.position;
+		var distance = heading.magnitude;
+		var direction = heading / distance;
+		distanceText.text = distance.ToString();
+		directionText.text = direction.ToString();
+
+	}
+}
