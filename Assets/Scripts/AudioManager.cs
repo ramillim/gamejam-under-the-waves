6f4198aa50bb5132950b50f5543ec9bf0b2e2313
Ramil Lim <ramil.lim@gmail.com>
2017-01-21 12:27:33 -0500

    Wire up the AudioManager and sfx.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,79 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
+public class AudioManager : MonoBehaviour
+{
+	public AudioSource nAudioSource;
 
-    public AudioSource nAudioSource;
+	public MyParamsAudio GetParams(Vector2 v2SourceLoc, Vector2 v2TargetLoc, float nGridLengthX, float nGridLengthY)
+	{
+		// Default Var, can change
+		float nDelayScalar = 5;
+		float nPitchDefault = 5;
+		float nPitchDelta = (float)0.1;
 
-    // public GameObject Gameobject?
+		// Storing Source Var
+		float nSourceX = v2SourceLoc.x;
+		float nSourceY = v2SourceLoc.y;
 
-	// Use this for initialization
-	void Start () {
-		
+		float nTargetX = v2TargetLoc.x;
+		float nTargetY = v2TargetLoc.y;
+
+		// Finding Deltas
+		float nDeltaX = nTargetX - nSourceX;
+		float nDeltaY = nTargetY - nSourceY;
+		float nDistance = Mathf.Sqrt(Mathf.Pow(nDeltaX, 2) + Mathf.Pow(nDeltaY, 2));
+		float nGridMaxLength = Mathf.Sqrt(Mathf.Pow(nGridLengthX, 2) + Mathf.Pow(nGridLengthY, 2));
+
+		// Getting Params
+		MyParamsAudio nParamAudio = new MyParamsAudio();
+		nParamAudio.Pan = nDeltaX / nGridLengthX;
+		nParamAudio.Delay = nDelayScalar * (nDistance / nGridMaxLength);
+		nParamAudio.Pitch = nPitchDefault + nPitchDelta * (nDeltaY / nGridLengthY);
+
+		return nParamAudio;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void AudioPlay(float nPan = 0, float nPitch = 1, float nDelay = 0, float nVol = 1)
+	{
+		nAudioSource.panStereo = nPan;
+		nAudioSource.volume = nVol;
+		nAudioSource.pitch = nPitch;
+
+		nAudioSource.PlayDelayed(nDelay);
 	}
 
-    public MyParamsAudio GetParams(Vector2 v2SourceLoc, Vector2 v3TargetLoc, float nGridLengthX, float nGridLengthY)
-    {
-        // Default Var, can change
-        float nDelayScalar = 5;
-        float nPitchDefault = 5;
-        float nPitchDelta = (float) 0.1;
+	public void AudioPlay(MyParamsAudio nParams)
+	{
+		float nPan = nParams.Pan;
+		float nPitch = nParams.Pitch;
+		float nDelay = nParams.Delay;
+		float nVol = nParams.Volume;
 
-        // Storing Source Var
-        float nSourceX = v2SourceLoc.x;
-        float nSourceY = v2SourceLoc.y;
+		nAudioSource.panStereo = nPan;
+		nAudioSource.volume = nVol;
+		nAudioSource.pitch = nPitch;
 
-        float nTargetX = v3TargetLoc.x;
-        float nTargetY = v3TargetLoc.y;
-
-        // Finding Deltas
-        float nDeltaX = nTargetX - nSourceX;
-        float nDeltaY = nTargetY - nSourceY;
-        float nDistance = Mathf.Sqrt(Mathf.Pow(nDeltaX, 2) + Mathf.Pow(nDeltaY, 2));
-        float nGridMaxLength = Mathf.Sqrt(Mathf.Pow(nGridLengthX, 2) + Mathf.Pow(nGridLengthY, 2));
-        
-        // Getting Params
-        MyParamsAudio nParamAudio = new MyParamsAudio();
-        nParamAudio.Pan = nDeltaX / nGridLengthX;
-        nParamAudio.Delay = nDelayScalar* (nDistance / nGridMaxLength);
-        nParamAudio.Pitch = nPitchDefault + nPitchDelta*(nDeltaY/nGridLengthY);
-
-        return nParamAudio;
-    }
-
-    public void AudioPlay(float nPan = 0, float nPitch = 1, float nDelay = 0, float nVol = 1) 
-    {
-        nAudioSource.panStereo = nPan;
-        nAudioSource.volume = nVol;
-        nAudioSource.pitch = nPitch;
-
-        nAudioSource.PlayDelayed(nDelay);
-    }
-
-    public void AudioPlay(MyParamsAudio nParams)
-    {
-        float nPan = nParams.Pan;
-        float nPitch = nParams.Pitch;
-        float nDelay = nParams.Delay;
-        float nVol = nParams.Volume;
-
-        nAudioSource.panStereo = nPan;
-        nAudioSource.volume = nVol;
-        nAudioSource.pitch = nPitch;
-
-        nAudioSource.PlayDelayed(nDelay);
-    }
+		nAudioSource.PlayDelayed(nDelay);
+	}
 }
 
 public class MyParamsAudio
 {
-    public float Pan;
-    public float Volume;
-    public float Pitch;
-    public float Delay;
+	public float Pan;
+	public float Volume;
+	public float Pitch;
+	public float Delay;
 }
--- Assets/Scripts/DirectionFinder.cs
+++ Assets/Scripts/DirectionFinder.cs
@@ -5,22 +5,39 @@ using UnityEngine.UI;
 
 public class DirectionFinder : MonoBehaviour
 {
-	private GameObject target;
 	public Text distanceText;
 	public Text directionText;
+
+	private GameObject target;
+	private AudioManager audioManager;
+
+	void Awake()
+	{
+		audioManager = GetComponent<AudioManager>();
+	}
 
 	void Start()
 	{
 		target = GameObject.FindWithTag("Target");
+		AudioSource aSource = GetComponent<AudioSource>();
 
+		//aSource.Play();
+		DropSonar();
 	}
 
 	void Update()
+	{
+		//DropSonar();
+	}
+
+	private void DropSonar()
 	{
 		var targetPos = target.transform.position;
 		Vector2 heading = targetPos - transform.position;
 		var distance = heading.magnitude;
 		UpdateDebug(distance, heading);
+		//PlaySonar();
+		PlayEcho(transform.position, targetPos);
 	}
 
 	private void UpdateDebug(float distance, Vector2 heading)
@@ -28,4 +45,19 @@ public class DirectionFinder : MonoBehaviour
 		distanceText.text = distance.ToString();
 		directionText.text = heading.ToString();
 	}
+
+	/// <summary>
+	/// Initial ping sound when click occurs.
+	/// </summary>
+	private void PlaySonar()
+	{
+		// Set up multiple audio sources.
+	}
+
+	private void PlayEcho(Vector2 sourceVector, Vector2 targetVector)
+	{
+		var audioParams = audioManager.GetParams(sourceVector, targetVector, 960, 960);
+		audioManager.AudioPlay(audioParams);
+	}
+
 }
